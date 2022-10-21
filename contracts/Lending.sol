3506eb21ef97185f14900fa9f662c61c02dbc0bf
Kefa698 <kefaombongi07@gmail.com>
2022-10-21 14:40:12 +0300

    fixed

--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "hardhat/console.sol";
+
 
 error TransferFailed();
 error TokenNotAllowed(address token);
